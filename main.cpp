3b503be64322984a4add5d4dd705c93c7e5474b8
Frank Zhao <bz2190@nyu.edu>
2023-12-11 11:11:29 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <Adafruit_CircuitPlayground.h>
+
+float X, Y, Z;
+
+
+void setup() {
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+}
+
+void loop() {
+
+  X = CircuitPlayground.motionX();
+  Y = CircuitPlayground.motionY();
+  Z = CircuitPlayground.motionZ();
+
+  Serial.print("X: ");
+  Serial.print(X);
+  Serial.print("  Y: ");
+  Serial.print(Y);
+  Serial.print("  Z: ");
+  Serial.println(Z);
+
+  delay(100);
+  if (X < 0){
+    CircuitPlayground.setPixelColor(3,   0, 128, 128);
+  }
+  else{
+    CircuitPlayground.clearPixels();
+  }
+}
