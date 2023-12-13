e127df64b1507e215bdbc816adf723d62e709996
Frank Zhao <bz2190@nyu.edu>
2023-12-12 22:31:31 -0500

    finsh 6 gestures

--- main.cpp
+++ main.cpp
@@ -12,17 +12,32 @@ bool start(){
   }
 }
 
-// we can add facing direction later
-bool stage1(float X, float Y, float Z){
-  return (X < 0);
+bool up(float X, float Y, float Z){
+  return (Z > 9);
 }
 
-bool stage2(float X, float Y, float Z){
-  return (X > 0);
+bool down(float X, float Y, float Z){
+  return (Z < -9);
 }
 
-bool stage3(float X, float Y, float Z){
-  return (Z < -8);
+bool left(float X, float Y, float Z){
+  return (Y > 9);
+}
+
+bool right(float X, float Y, float Z){
+  return (Y < -9);
+}
+
+bool forward(float X, float Y, float Z){
+  return (X > 9);
+}
+
+bool backward(float X, float Y, float Z){
+  return (X < -9);
+}
+
+bool shake(float X, float Y, float Z){
+  return (Y > 15 || Y < -15);
 }
 
 void setup() {
@@ -34,6 +49,7 @@ void setup() {
 }
 
 void loop() {
+  delay(100);
 
   X = CircuitPlayground.motionX();
   Y = CircuitPlayground.motionY();
@@ -48,25 +64,69 @@ void loop() {
   Serial.print("Stage  ");
   Serial.println(stage);
 
-  // 10 stage checks
-  if (stage == 2){
-    CircuitPlayground.setPixelColor(2, 0, 128, 128);
+  // 10 gesture stages check
+  if (stage == -1){
+    CircuitPlayground.setPixelColor(0, 255,   0,   0);
   }
-  if (stage == 3){
+  if (stage == -2){
+    CircuitPlayground.setPixelColor(1, 128, 128,   0);
+  }
+  if (stage == -3){
+    CircuitPlayground.setPixelColor(2,   0, 255,   0);
+  }
+  if (stage == -4){
+    CircuitPlayground.setPixelColor(3,   0, 128, 128);
+  }
+  if (stage == -5){
+    CircuitPlayground.setPixelColor(4,   0,   0, 255);
+  }
+  if (stage == -6){
     CircuitPlayground.setPixelColor(5, 0xFF0000);
   }
+  if (stage == -7){
+    CircuitPlayground.setPixelColor(6, 0x808000);
+  }
+  if (stage == -8){
+    CircuitPlayground.setPixelColor(7, 0x00FF00);
+  }
+  if (stage == -9){
+    CircuitPlayground.setPixelColor(8, 0x008080);
+  }
+  if (stage == -10){
+    CircuitPlayground.setPixelColor(9, 0x0000FF);
+  }
 
-  delay(100);
+
   if (start()){
     if (stage == 0){
-      if (stage1(X, Y, Z)) {stage = 1;}
+      if (up(X, Y, Z)) { stage = 1; }
     }
     if (stage == 1){
-      if (stage2(X, Y, Z)) {stage = 2;}
+      if (left(X, Y, Z)) { stage = 3; }
+      if (right(X, Y, Z)) { stage = 4; }
+      if (forward(X, Y, Z)) { stage = 5; }
+      if (backward(X, Y, Z)) { stage = 6; }
+      if (shake(X, Y, Z)) { stage = -5; }
     }
     if (stage == 2){
-      if (stage3(X, Y, Z)) {stage = 3; }
+      
     }
+    if (stage == 3){
+      if (down(X, Y, Z)) { stage = -1; }
+    }
+    if (stage == 4){
+      if (down(X, Y, Z)) { stage = -2; }
+    }
+    if (stage == 5){
+      if (down(X, Y, Z)) { stage = -3; }
+    }
+    if (stage == 6){
+      if (down(X, Y, Z)) { stage = -4; }
+    }
+  }
+  else{ // This is only for testing, in project we only need to reach one gesture stage
+    CircuitPlayground.clearPixels();
+    stage = 0;
   }
   
 }
